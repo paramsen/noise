d913c38017dbbcbdec6e9574394845e7cb678aa8
Pär Nils Amsen <par.nils.amsen@gmail.com>
2017-06-24 14:28:15 +0200

    gitmodule kissfft, mv app->library gradle module, setup CMakeLists.txt for kissfft

--- library/src/main/native/bridge.cpp
+++ library/src/main/native/bridge.cpp
@@ -1,13 +1,11 @@
 #include <jni.h>
 #include <string>
+#include "kiss_fft.h"
 
 extern "C"
 JNIEXPORT jstring
 
-JNICALL
-Java_com_paramsen_kissfft_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
+JNICALL Java_com_paramsen_kissfft_MainActivity_stringFromJNI(JNIEnv *env, jobject /*this*/) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
