a91ef16cbd303f287d239fe22f72f5f1dea4ce23
Pär Amsen <par.nils.amsen@gmail.com>
2019-11-07 16:26:42 +0100

    convert from C++ to C, use explicit APP_STL=none for conflictless use of library

--- noise/src/main/native/noise.c
+++ noise/src/main/native/noise.c
@@ -1,10 +1,7 @@
 #include <jni.h>
-#include <iostream>
 #include <android/log.h>
 #include <kiss_fft.h>
 #include "tools/kiss_fftr.h"
-
-extern "C" {
 
 typedef struct {
     kiss_fft_cpx *result;
@@ -18,11 +15,12 @@ typedef struct {
 } NoiseOptimizedImaginaryCfg;
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_realThreadSafe(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_realThreadSafe(JNIEnv *env, jclass jThis,
                                                          jfloatArray jInput,
                                                          jfloatArray jOutput) {
-    jsize inSize = env->GetArrayLength(jInput);
-    jsize outSize = env->GetArrayLength(jOutput);
+    jsize inSize = (*env)->GetArrayLength(env, jInput);
+
+    jsize outSize = (*env)->GetArrayLength(env, jOutput);
 
     if (outSize != inSize + 2)
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
@@ -31,8 +29,8 @@ Java_com_paramsen_noise_NoiseNativeBridge_realThreadSafe(JNIEnv *env, jobject jT
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
                             "kissfft require input length to be even");
 
-    float *input = env->GetFloatArrayElements(jInput, 0);
-    jfloat *output = env->GetFloatArrayElements(jOutput, 0);
+    float *input = (*env)->GetFloatArrayElements(env, jInput, 0);
+    jfloat *output = (*env)->GetFloatArrayElements(env, jOutput, 0);
 
     kiss_fftr_cfg config = kiss_fftr_alloc(inSize, 0, 0, 0);
     kiss_fft_cpx *result = (kiss_fft_cpx *) malloc(sizeof(kiss_fft_cpx) * outSize);
@@ -43,15 +41,15 @@ Java_com_paramsen_noise_NoiseNativeBridge_realThreadSafe(JNIEnv *env, jobject jT
         output[i * 2 + 1] = result[i].i;
     }
 
-    env->SetFloatArrayRegion(jOutput, 0, outSize, output);
-    env->ReleaseFloatArrayElements(jInput, input, 0);
-    env->ReleaseFloatArrayElements(jOutput, output, 0);
+    (*env)->SetFloatArrayRegion(env, jOutput, 0, outSize, output);
+    (*env)->ReleaseFloatArrayElements(env, jInput, input, 0);
+    (*env)->ReleaseFloatArrayElements(env, jOutput, output, 0);
     free(config);
     free(result);
 }
 
 JNIEXPORT jlong JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfg(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfg(JNIEnv *env, jclass jThis,
                                                            jint inSize) {
     NoiseOptimizedRealCfg *cfg = (NoiseOptimizedRealCfg *) malloc(sizeof(NoiseOptimizedRealCfg));
     cfg->config = kiss_fftr_alloc(inSize, 0, 0, 0);
@@ -61,7 +59,7 @@ Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfg(JNIEnv *env, jobject 
 }
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfgDispose(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfgDispose(JNIEnv *env, jclass jThis,
                                                                   jlong cfgPointer) {
     NoiseOptimizedRealCfg *cfg = (NoiseOptimizedRealCfg *) cfgPointer;
 
@@ -71,11 +69,11 @@ Java_com_paramsen_noise_NoiseNativeBridge_realOptimizedCfgDispose(JNIEnv *env, j
 }
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_realOptimized(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_realOptimized(JNIEnv *env, jclass jThis,
                                                         jfloatArray jInput,
                                                         jfloatArray jOutput, jlong cfgPointer) {
-    jsize inSize = env->GetArrayLength(jInput);
-    jsize outSize = env->GetArrayLength(jOutput);
+    jsize inSize = (*env)->GetArrayLength(env, jInput);
+    jsize outSize = (*env)->GetArrayLength(env, jOutput);
 
     if (outSize != inSize + 2)
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
@@ -84,8 +82,8 @@ Java_com_paramsen_noise_NoiseNativeBridge_realOptimized(JNIEnv *env, jobject jTh
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
                             "kissfft require input length to be even");
 
-    float *input = env->GetFloatArrayElements(jInput, 0);
-    jfloat *output = env->GetFloatArrayElements(jOutput, 0);
+    float *input = (*env)->GetFloatArrayElements(env, jInput, 0);
+    jfloat *output = (*env)->GetFloatArrayElements(env, jOutput, 0);
     NoiseOptimizedRealCfg *cfg = (NoiseOptimizedRealCfg *) cfgPointer;
 
     kiss_fftr(cfg->config, input, cfg->result);
@@ -95,23 +93,23 @@ Java_com_paramsen_noise_NoiseNativeBridge_realOptimized(JNIEnv *env, jobject jTh
         output[i * 2 + 1] = cfg->result[i].i;
     }
 
-    env->ReleaseFloatArrayElements(jInput, input, 0);
-    env->ReleaseFloatArrayElements(jOutput, output, 0);
+    (*env)->ReleaseFloatArrayElements(env, jInput, input, 0);
+    (*env)->ReleaseFloatArrayElements(env, jOutput, output, 0);
 }
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_imaginaryThreadSafe(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_imaginaryThreadSafe(JNIEnv *env, jclass jThis,
                                                               jfloatArray jInput,
                                                               jfloatArray jOutput) {
-    jsize inSize = env->GetArrayLength(jInput);
-    jsize outSize = env->GetArrayLength(jOutput);
+    jsize inSize = (*env)->GetArrayLength(env, jInput);
+    jsize outSize = (*env)->GetArrayLength(env, jOutput);
 
     if (outSize != inSize)
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
                             "output len must equal input len. Read javadoc.");
 
-    float *input = env->GetFloatArrayElements(jInput, 0);
-    float *output = env->GetFloatArrayElements(jOutput, 0);
+    float *input = (*env)->GetFloatArrayElements(env, jInput, 0);
+    float *output = (*env)->GetFloatArrayElements(env, jOutput, 0);
     kiss_fft_cpx *fftInput = (kiss_fft_cpx *) malloc(sizeof(kiss_fft_cpx) * inSize / 2);
 
     for (int i = 0; i < inSize / 2; i++) {
@@ -128,16 +126,16 @@ Java_com_paramsen_noise_NoiseNativeBridge_imaginaryThreadSafe(JNIEnv *env, jobje
         output[i * 2 + 1] = result[i].i;
     }
 
-    env->SetFloatArrayRegion(jOutput, 0, outSize, output);
-    env->ReleaseFloatArrayElements(jInput, input, 0);
-    env->ReleaseFloatArrayElements(jOutput, output, 0);
+    (*env)->SetFloatArrayRegion(env, jOutput, 0, outSize, output);
+    (*env)->ReleaseFloatArrayElements(env, jInput, input, 0);
+    (*env)->ReleaseFloatArrayElements(env, jOutput, output, 0);
     free(config);
     free(fftInput);
     free(result);
 }
 
 JNIEXPORT jlong JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfg(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfg(JNIEnv *env, jclass jThis,
                                                                 jint inSize) {
     NoiseOptimizedImaginaryCfg *cfg = (NoiseOptimizedImaginaryCfg *) malloc(
             sizeof(NoiseOptimizedImaginaryCfg));
@@ -149,7 +147,7 @@ Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfg(JNIEnv *env, job
 }
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfgDispose(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfgDispose(JNIEnv *env, jclass jThis,
                                                                        jlong cfgPointer) {
     NoiseOptimizedImaginaryCfg *cfg = (NoiseOptimizedImaginaryCfg *) cfgPointer;
 
@@ -160,19 +158,20 @@ Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimizedCfgDispose(JNIEnv *e
 }
 
 JNIEXPORT void JNICALL
-Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimized(JNIEnv *env, jobject jThis,
+Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimized(JNIEnv *env, jclass jThis,
                                                              jfloatArray jInput,
                                                              jfloatArray jOutput,
                                                              jlong cfgPointer) {
-    jsize inSize = env->GetArrayLength(jInput);
-    jsize outSize = env->GetArrayLength(jOutput);
+    jsize inSize = (*env)->GetArrayLength(env, jInput);
+    jsize outSize = (*env)->GetArrayLength(env, jOutput);
 
     if (outSize != inSize)
         __android_log_print(ANDROID_LOG_ERROR, "NATIVE_NOISE",
-                            "output len (%d) must equal input len (%d). Read javadoc.", outSize, inSize);
+                            "output len (%d) must equal input len (%d). Read javadoc.", outSize,
+                            inSize);
 
-    float *input = env->GetFloatArrayElements(jInput, 0);
-    float *output = env->GetFloatArrayElements(jOutput, 0);
+    float *input = (*env)->GetFloatArrayElements(env, jInput, 0);
+    float *output = (*env)->GetFloatArrayElements(env, jOutput, 0);
     NoiseOptimizedImaginaryCfg *cfg = (NoiseOptimizedImaginaryCfg *) cfgPointer;
 
     for (int i = 0; i < inSize / 2; i++) {
@@ -187,8 +186,7 @@ Java_com_paramsen_noise_NoiseNativeBridge_imaginaryOptimized(JNIEnv *env, jobjec
         output[i * 2 + 1] = cfg->result[i].i;
     }
 
-    env->SetFloatArrayRegion(jOutput, 0, outSize, output);
-    env->ReleaseFloatArrayElements(jInput, input, 0);
-    env->ReleaseFloatArrayElements(jOutput, output, 0);
-}
+    (*env)->SetFloatArrayRegion(env, jOutput, 0, outSize, output);
+    (*env)->ReleaseFloatArrayElements(env, jInput, input, 0);
+    (*env)->ReleaseFloatArrayElements(env, jOutput, output, 0);
 }
