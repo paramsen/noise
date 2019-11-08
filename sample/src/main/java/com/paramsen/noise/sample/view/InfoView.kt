0cccb4caaa0c7d31b5c76ec6e61805f937c4399e
Pär Amsen <par.nils.amsen@gmail.com>
2019-11-08 14:52:49 +0100

    update sample app to androidX jetpack components

--- sample/src/main/java/com/paramsen/noise/sample/view/InfoView.kt
+++ sample/src/main/java/com/paramsen/noise/sample/view/InfoView.kt
@@ -5,12 +5,12 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.LOLLIPOP
-import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.animation.AccelerateInterpolator
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.paramsen.noise.sample.BuildConfig
 import com.paramsen.noise.sample.R
